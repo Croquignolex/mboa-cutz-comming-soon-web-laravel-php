b848f8fea361891ff066d2374e11b42270e147d8
Alex Stéphane NGOMBOL <gpetitalex10@gmail.com>
2020-08-17 18:03:01 +0100

    Refactor new description and useless files

--- resources/views/partials/info.blade.php
+++ resources/views/partials/info.blade.php
@@ -1,13 +1,10 @@
 <div class="s-info">
     <div class="row info-wrapper">
-
         <div class="col-seven tab-full info-main">
             <h1>{{ env('APP_NAME') }}.</h1>
-            <iframe width="100%" height="500" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3979.975159429634!2d9.69788616525108!3d4.025480148261639!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x106112c7426691ff%3A0x310e860af24cda5!2sBonapriso%2C%20Douala!5e0!3m2!1sfr!2scm!4v1597665163772!5m2!1sfr!2scm" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+            <iframe width="100%" height="400" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3979.975159429634!2d9.69788616525108!3d4.025480148261639!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x106112c7426691ff%3A0x310e860af24cda5!2sBonapriso%2C%20Douala!5e0!3m2!1sfr!2scm!4v1597665163772!5m2!1sfr!2scm" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
         </div>
-
         <div class="col-four tab-full pull-right info-contact">
-
             <div class="info-block">
                 <h3>{{ trans('home.can_contact_us_by') }}</h3>
                 <p>
@@ -15,16 +12,13 @@
                     <a href="tel:+18587338912" class="'info-phone">+237 690 467 995</a>
                 </p>
             </div>
-
             <div class="info-block">
                 <h3>{{ trans('home.our_office') }}</h3>
-
                 <p class="info-address">
                     Bonapriso<br>
                     Douala, CAMEROUN<br>
                 </p>
             </div>
-
             <div class="info-block">
                 <h3>{{ trans('home.find_us') }}</h3>
                 <ul class="info-social">
@@ -50,16 +44,5 @@
             </div>
         </div>
     </div>
-
-    <div class="row home-copyright">
-        <span>Copyright MBOA CUTZ 2020</span>
-        <span>
-                Design by
-                <a href="https://croquignolex-tikiton.dmsemergence.com/" target="_blank">
-                    Croquignolex Tikiton
-                </a>
-            </span>
-    </div>
-
     <div class="home-content__line"></div>
 </div>
